066a541a80dc16e07cac8a89300d44dfe7497a0b
Kacper Wardynski <kacper.wardynski@gmail.com>
2026-03-26 23:13:51 -0600

    deps: Add NimblePublisher, :makeup_elixir and :makeup_erlang as deps

--- mix.exs
+++ mix.exs
@@ -35,10 +35,8 @@ defmodule Website.MixProject do
       {:phoenix, "~> 1.7.21"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.1.1",
@@ -51,7 +49,14 @@ defmodule Website.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+
+      # blog deps
+      {:makeup_elixir, "~> 1.0"},
+      {:makeup_erlang, "~> 1.0"},
+      {:nimble_publisher, "~> 1.1"},
+      {:phoenix_live_view, "~> 1.0"},
+      {:tailwind, "~> 0.2", runtime: false}
     ]
   end
 
